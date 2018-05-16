b7cbb89093a949046c59071b9bdb70e9cff5d9f9
Kaari Strack <kaari@absolutedevops.io>
2018-05-16 13:35:59 +0100

    :bug: fixed bootstrap sprocket dropdown bug

--- PageSkimr/app/assets/javascripts/application.js
+++ PageSkimr/app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 //= require_tree .
